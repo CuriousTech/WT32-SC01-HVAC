6d72d750f8231c433aae07f16a2ef6363f252034
Greg Cunningham <greg@curioustech.net>
2023-12-04 08:38:45 -0500

    Saving space

--- Arduino/screensavers.h
+++ Arduino/screensavers.h
@@ -13,7 +13,6 @@ enum Saver
   SS_Clock,
   SS_Lines,
   SS_Boing,
-  SS_Cube,
   SS_Count, // Last one
 };
 
@@ -34,41 +33,15 @@ struct Ball
   int16_t color;
 };
 
-struct Point3d
-{
-  int x;
-  int y;
-  int z;
-};
-
-struct Point2d
-{
-  int x;
-  int y;
-};
-
-struct Line3d
-{
-  Point3d p0;
-  Point3d p1;
-};
-
-struct Line2d
-{
-  Point2d p0;
-  Point2d p1;
-};
 
 class ScreenSavers
 {
 #define LINES 50
 #define BALLS 8
-#define LINES3DREND 20 // max 170 for 2048 bytes
 #define BUFFER_SIZE 1024 // Caution: Also used by image loader (512 pixel width max)
 
 static_assert(sizeof(Ball) * BALLS < BUFFER_SIZE, "m_buffer size too small");
 static_assert(sizeof(Line) * LINES < BUFFER_SIZE, "m_buffer size too small");
-static_assert(sizeof(Line2d) * 2 * LINES3DREND < BUFFER_SIZE, "m_buffer size too small");
 
 public:
   ScreenSavers(){};
@@ -83,19 +56,6 @@ private:
   void cspoint(uint16_t &x2, uint16_t &y2, uint16_t x, uint16_t y, float angle, float size);
   void Lines(bool bInit);
   void Boing(bool bInit);
-  void Cube(bool bInit);
-  void SetVars(void);
-  void Transform(struct Line2d *ret, struct Line3d vec);
-
-  float xx, xy, xz;
-  float yx, yy, yz;
-  float zx, zy, zz;
-  
-  int Xan, Yan;
-  
-  int Xpos;
-  int Ypos;
-  int Zpos;
 };
 
 
