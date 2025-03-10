4e55317e945332eb518e74326c3b2fbac4e98e3b
Greg Cunningham <greg@curioustech.net>
2025-03-10 09:36:21 -0400

    Update eeMem.h

--- Arduino/eeMem.h
+++ Arduino/eeMem.h
@@ -20,7 +20,7 @@ struct flags_t
 class eeMem
 {
 public:
-  eeMem();
+  eeMem(){};
   bool init(void);
   bool check(void);
   bool update(void);
@@ -30,8 +30,8 @@ public:
 public:
   uint16_t size = EESIZE;            // if size changes, use defaults
   uint16_t sum = 0xAAAA;             // if sum is different from memory struct, write
-  char     szSSID[24] = "";  // Enter you WiFi router SSID
-  char     szSSIDPassword[24] = ""; // and password
+  char     szSSID[24] = "";          // Enter you WiFi router SSID
+  char     szSSIDPassword[24] = "";  // and password
   uint16_t coolTemp[2] = {850, 860}; // cool to temp *10 low/high F/C issue
   uint16_t heatTemp[2] = {740, 750}; // heat to temp *10 low/high F/C issue
   int8_t   cycleThresh[2] = {28, 8}; // temp range for cycle *10 [cool|heat] F/C issue
@@ -39,15 +39,15 @@ public:
   uint8_t  eHeatThresh = 33;        // degree threshold to switch to gas    F/C issue
   int8_t   tz = -5;                 // current timezone
   int8_t   adj;                     // temp sensor offset adjust by 0.1
-  uint16_t cycleMin = 60*4;         // min time to run a cycle in minutes
-  uint16_t cycleMax = 60*30;        // max time to run a cycle in minutes
-  uint16_t idleMin = 60*8;          // min time to not run in minutes
+  uint16_t cycleMin = 60*4;         // min time to run a cycle in seconds
+  uint16_t cycleMax = 60*30;        // max time to run a cycle in seconds
+  uint16_t idleMin = 60*8;          // min time to not run in seconds
   uint16_t fanPostDelay[2] = {60*2, 60*2}; // delay to run auto fan after [hp/cool] stops
   uint16_t fanPreTime[2] = {60*1, 60*1}; // fan pre-run before [cool/heat]
-  uint16_t overrideTime = 60*10;    // time used for an override in minutes
+  uint16_t overrideTime = 60*10;    // time used for an override in seconds
   uint16_t rhLevel[2] = {450, 750}; // rh low/high 45%, 75%
   int8_t   awayDelta[2] = {40, -40}; // temp offset in away mode[cool][heat] by 0.1
-  uint16_t awayTime = 60*8;         // time limit for away offset (in minutes)
+  uint16_t awayTime = 60*8;         // time limit for away offset (in seconds)
   uint8_t  hostIp[4] = {192,168,31,100}; // Device to read local forecast info
   uint16_t hostPort = 80;
   char     cityID[8] = "4291945";   // For OpenWeatherMap  4311646
@@ -56,24 +56,22 @@ public:
   uint8_t  fcDisplay = 46;          // number in forecasts (3 hours)
   uint8_t  fanAutoRun = 5;          // 5 minutes on
   uint8_t  fanWatts = 250;          // Blower motor watts
-  uint8_t  furnaceWatts = 140;// 220;      // 1.84A inducer motor mostly
+  uint8_t  furnaceWatts = 140;      // 1.84A inducer motor mostly
   uint8_t  humidWatts = 150;
   uint8_t  brightLevel[2] = {30, 100}; // brightness {dim, highest}
-  uint16_t ppkwh = 147;             // price per KWH in cents * 10000 (0.147)
+  uint16_t ppkwh = 150;             // price per KWH in cents * 10000 ($0.15)
   uint16_t ccf = 1190;              // nat gas cost per 1000 cubic feet in 10th of cents * 1000 ($1.190)
-  uint16_t cfm = 820;               // cubic feet per minute * 1000 of furnace (0.82)         // cubic feet per minute
+  uint16_t cfm = 820;               // cubic feet per minute * 1000 of furnace (0.82)
   uint16_t compressorWatts = 2600;  // compressorWatts
   uint16_t furnacePost = 110;       // furnace internal fan timer
-  uint16_t diffLimit = 300;         // in/out thermal differential limit. Set to 30 deg limit    F/C issue
+  uint16_t diffLimit = 200;         // in/out thermal differential limit. Set to 20 deg limit    F/C issue
   int16_t  fcOffset[2] = {-180,0};  // forecast offset adjust in minutes (cool/heat)
   uint16_t fanIdleMax = 60*4;       // fan idle max in minutes
   int16_t  sineOffset[2] = {0, 0};  // sine offset adjust (cool/heat)
   uint32_t sensorActive[8];         // sensor IDs for restart
-  uint8_t  reserved[320];
+  uint8_t  reserved[320];           // Note: To force an EEPROM update, just subtract 1 byte and flash again
   uint8_t  end;
 }; // 512 bytes
-
-static_assert(EESIZE <= 512, "EEPROM struct too big");
 
 extern eeMem ee;
 
