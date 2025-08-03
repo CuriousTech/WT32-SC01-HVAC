8ed73c549dab6de23b123386af9dd86758f28758
Greg Cunningham <greg@curioustech.net>
2025-08-02 21:06:18 -0400

    Update eeMem.h

--- Sensor/eeMem.h
+++ Sensor/eeMem.h
@@ -1,55 +1,56 @@
-#include "eeMem.h"
-#include <EEPROM.h>
+#ifndef EEMEM_H
+#define EEMEM_H
 
-void eeMem::init()
+#include <Arduino.h>
+
+struct eflags
 {
-  EEPROM.begin(EESIZE);
+  uint8_t PriEn:2;
+  uint8_t bPIR:1;
+  uint8_t bCall:1;
+  uint8_t bCF:1;
+  uint8_t bUseTime:1;
+  uint8_t bEnableOLED:1;
+  uint8_t res:1;
+};
 
-  uint8_t data[EESIZE];
-  uint16_t *pwTemp = (uint16_t *)data;
+#define EESIZE (offsetof(eeMem, end) - offsetof(eeMem, size) )
 
-  int addr = 0;
-  for(int i = 0; i < EESIZE; i++, addr++)
-  {
-    data[i] = EEPROM.read( addr );
-  }
-
-  if(pwTemp[0] != EESIZE) return; // revert to defaults if struct size changes
-  uint16_t sum = pwTemp[1];
-  pwTemp[1] = 0;
-  pwTemp[1] = Fletcher16(data, EESIZE );
-  if(pwTemp[1] != sum) return; // revert to defaults if sum fails
-  memcpy(this + offsetof(eeMem, size), data, EESIZE );
-}
-
-void eeMem::update() // write the settings if changed
+class eeMem
 {
-  uint16_t old_sum = ee.sum;
-  ee.sum = 0;
-  ee.sum = Fletcher16((uint8_t*)this + offsetof(eeMem, size), EESIZE);
+public:
+  eeMem(){};
+  void init(void);
+  void update(void);
+private:
+  uint16_t Fletcher16( uint8_t* data, int count);
+public:
+  uint16_t  size = EESIZE;          // if size changes, use defauls
+  uint16_t  sum = 0xAAAA;           // if sum is diiferent from memory struct, write
+  char      szSSID[32] = "CTech001";
+  char      szSSIDPassword[64] = "m27c4001WiFi";
+  int8_t    tz = -5;               // Timezone offset
+  eflags    e = {0,0,1,1,0,0,0}; // PirEn, bPIR, bCall, bCF, bUseTime, bEnableOLED, res
+  char      szName[32] = "Sensor2";
+  uint32_t  sensorIDxx = 0;
+  int8_t    tempCal = 0;
+  uint16_t  sendRate = 15;
+  uint16_t  logRate = 60;
+  char      szControlPassword[32] = "esp8266ct";
+  uint8_t   hostIP[4] = {192,168,31,100};
+  uint16_t  hostPort = 80;
+  uint8_t   hvacIP[4] = {192,168,31,46};
+  uint32_t  time_off = 0;
+  uint32_t  sleep = 30;
+  uint32_t  priSecs = 60*5;
+  uint8_t   pirPin = 12;
+  uint16_t  wAlertLevel[16] =  {320, 1000, 0, 900, 0, 1000, 0, 10, 0, 20, 0, 1000, 0, 1000, 0, 1000}; // alert levels L/H
+  int8_t    rhCal = 0;
+  uint8_t   weight = 1;
+  uint8_t   res[30];
+  uint8_t   end;
+};
 
-  if(old_sum == ee.sum)
-    return; // Nothing has changed?
+extern eeMem ee;
 
-  uint16_t addr = 0;
-  uint8_t *pData = (uint8_t *)this + offsetof(eeMem, size);
-  for(int i = 0; i < EESIZE; i++, addr++)
-  {
-    EEPROM.write(addr, pData[i] );
-  }
-  EEPROM.commit();
-}
-
-uint16_t eeMem::Fletcher16( uint8_t* data, int count)
-{
-   uint16_t sum1 = 0;
-   uint16_t sum2 = 0;
-
-   for( int index = 0; index < count; ++index )
-   {
-      sum1 = (sum1 + data[index]) % 255;
-      sum2 = (sum2 + sum1) % 255;
-   }
-
-   return (sum2 << 8) | sum1;
-}
+#endif // EEMEM_H
