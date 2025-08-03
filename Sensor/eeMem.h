b12e5fd053630f4f069eaf2005799e8a27b83133
Greg Cunningham <greg@curioustech.net>
2025-08-02 21:07:30 -0400

    Update eeMem.h

--- Sensor/eeMem.h
+++ Sensor/eeMem.h
@@ -27,16 +27,16 @@ private:
 public:
   uint16_t  size = EESIZE;          // if size changes, use defauls
   uint16_t  sum = 0xAAAA;           // if sum is diiferent from memory struct, write
-  char      szSSID[32] = "CTech001";
-  char      szSSIDPassword[64] = "m27c4001WiFi";
+  char      szSSID[32] = "";
+  char      szSSIDPassword[64] = "";
   int8_t    tz = -5;               // Timezone offset
   eflags    e = {0,0,1,1,0,0,0}; // PirEn, bPIR, bCall, bCF, bUseTime, bEnableOLED, res
-  char      szName[32] = "Sensor2";
+  char      szName[32] = "Sensor1";
   uint32_t  sensorIDxx = 0;
   int8_t    tempCal = 0;
   uint16_t  sendRate = 15;
   uint16_t  logRate = 60;
-  char      szControlPassword[32] = "esp8266ct";
+  char      szControlPassword[32] = "password";
   uint8_t   hostIP[4] = {192,168,31,100};
   uint16_t  hostPort = 80;
   uint8_t   hvacIP[4] = {192,168,31,46};
