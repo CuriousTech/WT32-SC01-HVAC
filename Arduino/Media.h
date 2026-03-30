4c1b2c478c67e6fbb2cd8c05a50b1ffc6f449201
Greg Cunningham <greg@curioustech.net>
2026-03-30 11:53:31 -0400

    Update Media.h

--- Arduino/Media.h
+++ Arduino/Media.h
@@ -2,22 +2,13 @@
 #define MEDIA_H
 
 #include <Arduino.h>
+#include <TJpg_Decoder.h>   //https://github.com/Bodmer/TJpg_Decoder
+#define FS_NO_GLOBALS
 #include <FS.h>
-#include <PNGdec.h>  // From Library Manager
 
 #include <SPIFFS.h> // SPIFFS has no subdirs but takes less code space
 
 #define INTERNAL_FS SPIFFS // Make it the same as the partition scheme uploaded (SPIFFS, LittleFS, FFat)
-
-struct ImageCtrl
-{
-  int16_t x;
-  int16_t y;
-  int16_t srcX;
-  int16_t srcY;
-  int16_t w;
-  int16_t h;
-};
 
 class Media
 {
@@ -31,8 +22,8 @@ public:
   void setPath(char *szPath);
   uint32_t freeSpace(void);
 
-  void loadImage(String Name, uint16_t x, uint16_t y);
-  void loadImage(String Name, uint16_t x, uint16_t y, uint16_t srcX, uint16_t srcY, uint16_t w, uint16_t h);
+  void loadImage(String sName, uint16_t x, uint16_t y);
+  void loadImage(String sName, uint16_t x, uint16_t y, int16_t srcX, int16_t srcY, uint16_t w, uint16_t h);
 };
 
 extern Media media;
