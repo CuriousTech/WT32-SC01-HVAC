dd2d6e99fca00947f59363cf184fb842f91064bb
Greg Cunningham <9118395+CuriousTech@users.noreply.github.com>
2026-09-21 08:21:18 -0400

    Fixed boost param

--- Arduino/forecast.h
+++ Arduino/forecast.h
@@ -56,7 +56,7 @@ public:
   bool forecastPage(void);
   void forecastAnimate(void);
   void getMinMax(int16_t& tmin, int16_t& tmax, int8_t offset, int8_t range);
-  int16_t getCurrentTemp(int& shiftedTemp, uint8_t shiftMins, bool boost);
+  int16_t getCurrentTemp(int& shiftedTemp, uint8_t shiftMins, bool& boost);
   void drawIcon(uint8_t d, uint8_t h, uint16_t x);
 private:
   void _onConnect(AsyncClient* client);
